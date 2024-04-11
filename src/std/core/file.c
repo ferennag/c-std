ef952586fb08c138ad8f05c41ca417cace370cb8
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2024-04-11 19:09:04 -0400

    Add file handling lib

---
+++ src/std/core/file.c
@@ -0,0 +1,29 @@
+#include "file.h"
+
+#include <stdio.h>
+#include <std/core/logger.h>
+#include <std/core/memory.h>
+
+bool *file_read_binary(const char *path) {
+    FILE *file = fopen(path, "rb");
+
+    if (!file) {
+        LOG_ERROR("Unable to open file for reading: %s", path);
+        return false;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    void *buffer = memory_alloc(size);
+    u64 result = fread(buffer, size, 1, file);
+    fclose(file);
+
+    if (result != 1) {
+        LOG_ERROR("Unable to read from file: %s", path);
+        return false;
+    }
+
+    return buffer;
+}
---
+++ src/std/core/file.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "std/defines.h"
+
+bool *file_read_binary(const char *path);
